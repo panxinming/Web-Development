919d70d419a0d376c34cc7aa97e769bf43b39630
hhhh <83961129+panxinming@users.noreply.github.com>
2022-01-30 15:53:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,78 +1,105 @@
-'''
-In this example, we are going to look at creating, serving, and deploying a *very simple* webapp. In the following several lecture, we'll see how to add some interesting interactivity to our app. 
 
-# Prerequisites
+# import necessary package.
+from flask import Flask, g, render_template, request
+import sqlite3
 
-- You need to have the flask package installed in your PIC16B Anaconda environment. 
-- You need a Heroku account.
-- You need the Heroku command line interface: 
-    - To install, at the command line (for MacOS)
-    brew tap heroku/brew && brew install heroku
-
-- At the command line, run 
-    conda activate PIC16B
-
-# Local Preview
-
-At the command line: 
-
-export FLASK_ENV=development; flask run
-
-# Deployment
-
-*Note*: these notes are written for the version of the app that Phil is using, which is indeed called pic16b-minimal-demo. In order to make your own version, you would need to give the app a different name (because one with this name already exists on the internet now).
-
-Sign up for Heroku, create app called pic16b-minimal-demo
-
-```
-heroku login
-heroku git:remote -a pic16b-minimal-demo
-
-git add *.
-git commit -m'add files for heroku'
-git push heroku
-```
-
-Then, the website is at 
-https://pic16b-minimal-demo.herokuapp.com
-
-    
-# Sources
-
-This set of lecture notes is based in part on previous materials developed by [Erin George](https://www.math.ucla.edu/~egeo/) (UCLA Mathematics) and the tutorial [here](https://stackabuse.com/deploying-a-flask-application-to-heroku/). 
-'''
-
-from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
+
+def get_message_db():
+    # create a database which contains our messages.
+    g.message_db = sqlite3.connect("message_db.sqite") 
+    cursor = g.message_db.cursor()   
+    # SQL command CREATE a TABLE called messages IF NOT EXISTS
+    cursor.execute("create table if not exists messages(Id integer, handle text, message text)") 
+    cursor.close()
+    # Return the connection 
+    return g.message_db
+    
+
+def insert_message(request):
+    # collect the user's message and handle.
+    message = request.form['message']
+    handle = request.form['user']
+    # if user enter both text, then
+    if (message and handle):
+        # insert message into our database.
+        cursor = g.message_db.cursor()
+        cursor.execute("select Max(Id) from messages")
+        result = cursor.fetchone()[0]
+        # ensure that the ID number of each message is unique
+        if (result==None):
+            s = 1
+        else:
+            s = result+1
+        # insert Id, handle, message together.
+        g.sql = 'insert into messages(Id,handle,message) values('+str(s)+',"'+handle+'","'+message+'")'
+        cursor.execute(g.sql)
+        #  it is necessary to run db.commit() after inserting a row into db in order to ensure that your row insertion has been saved.
+        g.message_db.commit()
+        # close our database.
+        cursor.close()
+        g.message_db.close()
+    else:
+        # if user type nothing, then
+        g.sql =  "Message or handle is null ,please try again"
+    return g.sql
+
+
+
+def random_messages(n):
+    # connect to our database we just create.
+    g.message_db = sqlite3.connect("message_db.sqite") 
+    cursor = g.message_db.cursor()
+    # extract what we need.
+    cursor.execute("select message,handle from messages")
+    # get our text
+    result = cursor.fetchmany(n)
+    # close our database
+    cursor.close()
+    g.message_db.close()
+    return result
+
+
 @app.route("/")
-# simplest possible approach
-# def main():
-#     return "hi there!"
-
-
-# slightly less trivial
-# def main():
-#     return render_template("main.html")
-
-# A little fancier
-
-# @app.route("/")
 def main():
-    return render_template("main_better.html")
+    # main page is base.html
+    return render_template("base.html")
 
-# getting basic user data
-@app.route('/ask/', methods=['POST', 'GET'])
-def ask():
+
+@app.route('/view/', methods=['POST', 'GET'])
+def view():
     if request.method == 'GET':
-        return render_template('ask.html')
+        # reurn view.html if we get something.
+        return render_template('view.html')
     else:
         try:
-            return render_template('ask.html', name=request.form['name'], student=request.form['student'])
+            # select a int number.
+            n = int(request.form['number'])
+            # extract our message from database.
+            g.result = random_messages(n)
+            # view sepcific number of messages we want to show. And return us a thank you message.
+            return render_template('view.html',number = "thank you for submitting the message!")
         except:
-            return render_template('ask.html')
+            return render_template('view.html',number= "error")
 
-# 
-@app.route('/profile/<name>/')
-def hello_name(name):
-    return render_template('profile.html', name=name)
+
+@app.route('/submit/', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'GET':
+        # return submit.html.
+        return render_template('submit.html')
+    else:
+        try:
+            # create database.
+            get_message_db()
+            # insert our message into database.
+            insert_message(request)
+            return render_template('submit.html', user=request.form['user'], message=request.form['message'])
+        except:
+            return render_template('submit.html', user='error', message='error')
+
+
+if __name__ == '__main__':
+    app.run()
